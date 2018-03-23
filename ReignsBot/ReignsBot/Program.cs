38b20c9ffa079cf0e9d930978da85d52629386b2
Dyrem <beganbajrami@gmail.com>
2018-03-23 23:12:41 +0100

    Inizio

---
+++ ReignsBot/ReignsBot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReignsBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
