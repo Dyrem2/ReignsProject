109b715c27dbb6cc9f4928b9645730ee67b92d64
Friky <frikyfriks@gmail.com>
2018-03-24 18:18:39 +0100

    WebHook setting

--- ReignsBot/ReignsBot/Program.cs
+++ ReignsBot/ReignsBot/Program.cs
@@ -8,7 +8,12 @@ namespace ReignsBot
         static void Main(string[] args)
         {
             TelegramBotClient ClientBot = new TelegramBotClient("540336212:AAFDxXrlVvJ6rjRdUap4t0OYfUHkSJD99kw");
-            
+            string webhookUrl = Configuration["Settings:webhookUrl"];
+            int maxConnections = int.Parse (Configuration["Settings:MaxConnections"]);
+            UpdateType[] allowedUpdate = { UpdateType.MessageUpdate };
+
+            TelegramBotClient.setWebHook(webhookurls, maxConnections, allowedUpdate);
+            services.Addscoped<ITelegramBotClient>(ClientBot => telegramclient);
 
         }
     }
